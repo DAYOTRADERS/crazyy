336bee6eb06bca4349a4df53227a408367fce832
Arnoldekima <arnoldekima@gmail.com>
2025-11-09 16:54:23 +0300

    first commit

---
+++ packages/bot-skeleton/src/services/api/appIdResolver.js
@@ -0,0 +1,9 @@
+const AppIds = Object.freeze({
+    'deriv.app': '101644', // TODO: [app-link-refactor] - Remove backwards compatibility for `deriv.app`
+    'staging.deriv.app': '101644',
+    'app.deriv.com': '101644',
+    'staging-app.deriv.com': '101644',
+    'goon-puce.vercel.app': '101644',
+});
+
+export default AppIds;
